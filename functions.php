c9c522bee5e3cba0e0f836f76a6bd8baa3ddb013
Matt Vickers <matt@envexlabs.com>
2011-07-20 13:44:00 -0500

    Properly loading the scripts now

--- functions.php
+++ functions.php
@@ -13,3 +13,25 @@ add_theme_support('automatic-feed-links');
 */
 
 add_filter( 'show_admin_bar', '__return_false' );
+
+/*
+
+	Load up the scripts
+
+*/
+
+function envex_scripts(){
+	
+	if(is_admin()){ return; }
+	
+	wp_deregister_script('jquery');
+	
+	wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js', false, '1.6.2');
+	wp_enqueue_script('jquery');
+	
+	wp_register_script('main', get_bloginfo('template_url') . 'inc/js/main.js', false, '1.0', true);
+	wp_enqueue_script('main');
+	
+}
+
+add_action('init', 'envex_scripts');
