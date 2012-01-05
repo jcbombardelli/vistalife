7449d5dee9ab02722a342c09343b0f6ae2496049
Matt Vickers <matt@envexlabs.com>
2012-01-05 10:51:47 -0600

    Added less.js because that's how I roll now

--- header.php
+++ header.php
@@ -15,8 +15,22 @@
 		
 		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 		
-		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/inc/css/compressor.php" type="text/css" media="screen" />
-				
+		<!-- 
+		
+			less.js is only used for dev.
+			
+			You can remove line 26 once the site is ready to go production.
+			
+			You will also remove the less.js script on line 32 becauase
+			you'll be compiling all the less files into one nice css file
+			
+		-->
+		
+		<script type="text/javascript"> less = { env: 'development' }; </script>
+		
+		<link rel="stylesheet/less" type="text/css" href="<?php bloginfo('template_url'); ?>/inc/css/main.less">
+		<script src="https://raw.github.com/cloudhead/less.js/master/dist/less-1.1.6.min.js" type="text/javascript"></script>
+						
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 		
 		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
