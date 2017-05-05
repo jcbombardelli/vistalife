8f531af501b7fc973b83d1ba196d53f8b690315b
Bruno Geronimo <contato@brunogeronimo.com.br>
2017-05-04 22:45:42 -0300

    inclusão módulo recaptcha

--- scripts/email.php
+++ scripts/email.php
@@ -1,4 +1,21 @@
 <?php
+
+	require_once(__DIR__."/recaptcha.php");
+	$secret = "6LeJBiAUAAAAAAThsWqlUVpNzdJBf4r9E14uBPpS";
+	$response = null;
+	$reCaptcha = new ReCaptcha($secret);
+	if ($_POST["g-recaptcha-response"]) {
+		$response = $reCaptcha->verifyResponse(
+	        $_SERVER["REMOTE_ADDR"],
+	        $_POST["g-recaptcha-response"]
+	    );
+	}
+	echo "<pre>";
+	print_r($response);
+	echo "</pre>";
+
+
+
 	$name = $_POST['name'];
 	$phone = $_POST['phone'];
 	$_replyto = $_POST['_replyto'];
