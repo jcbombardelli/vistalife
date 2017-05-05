0a12dccaf2ad42edc1b397a614c2c62e51964b63
Bruno Geronimo <contato@brunogeronimo.com.br>
2017-05-04 22:32:20 -0300

    Script de envio de e-mail corrigido

--- scripts/email.php
+++ scripts/email.php
@@ -1,14 +1,20 @@
 <?php
-	$to = "hi@bruno.works";
-	$from = "hi@bruno.works";
-	$subject = "Contato - Site";
-	$body = "";
-
 	$name = $_POST['name'];
 	$phone = $_POST['phone'];
 	$_replyto = $_POST['_replyto'];
 	$message = $_POST['message'];
 
-	$body = "Nome: $name\nTelefone: $phone\nResponder para: $_replyto\nMensagem: $message";
+
+	$to = "hi@bruno.works";
+	$from = "hi@bruno.works";
+	$headers = "From: hi@bruno.works\r\nReply-To: $_replyto";
+	$subject = "Contato - Site";
+	$body = "";
+
+
+	$body = "Nome: $name<br/>"
+			."Telefone: $phone<br/>"
+			."Responder para: $_replyto<br/>"
+			."Mensagem: $message";
 	mail($to, $subject, $body);
 ?>
